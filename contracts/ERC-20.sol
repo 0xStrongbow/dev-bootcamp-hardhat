9aa597ab6be96823dd07295d23c8fda3a41535ff
0xStrongbow <[redacted]>
2021-11-25 16:05:27 -0700

    Fixed indentation

--- contracts/ERC-20.sol
+++ contracts/ERC-20.sol
@@ -97,13 +97,13 @@ contract ERC20Basic is IERC20 {
  
 library SafeMath {
     function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-      assert(b <= a);
-      return a - b;
+        assert(b <= a);
+        return a - b;
     }
  
     function add(uint256 a, uint256 b) internal pure returns (uint256) {
-      uint256 c = a + b;
-      assert(c >= a);
-      return c;
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
     }
 }
